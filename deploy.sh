0a1dd3204f7c43125992e73567e0429c41520e83
artcunha <arthuromcunha@gmail.com>
2020-10-09 22:37:21 +0100

    rebuilding site Fri, Oct  9, 2020 10:37:21 PM

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,14 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 hugo -t hugo-PaperMod
 
 # Go To Public folder
-cd public
-
-# Add changes to git.
-git add .
+cd docs
 
 # Commit changes.
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
-git commit -m "$msg"
+git commit -a -m "$msg"
 
 # Push source and build repos.
 git push origin master
